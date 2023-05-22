d4d507820090d84f80016e17a3b49bcb6ded4401
hyunboklee <gusqhrzkq@naver.com>
2023-05-22 16:16:23 +0900

    Feat : project create

---
+++ src/main/java/com/example/blindover_server/BlindoverServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.blindover_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindoverServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlindoverServerApplication.class, args);
+    }
+
+}
