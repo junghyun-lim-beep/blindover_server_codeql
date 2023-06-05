70e7f83fa62e952709450a0e2cf98e73d82ae759
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-06-05 15:52:08 +0900

    Feat : requirememts.txt 설정 및 deepleaning 모델 시도 중

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
+from torchvision.transforms import ToTensor
+from PIL import Image
 import torch
 import os
+
+from loguru import logger
+
+
+model = torch.load('shufflenet_weight.pt', map_location=torch.device('cpu'))
+
+app = FastAPI()
+
+
+@app.post("/photo")
+async def create_upload_file(file: UploadFile = File(...)):
+
+  ################################파일 업로드 부분#####################################
+  # UPLOAD_DIR = "./photo"  # 이미지를 저장할 서버 경로
+    
+  # content = await file.read()
+  # filename = "test.jpg"  # uuid로 유니크한 파일명으로 변경
+  # with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
+  #     fp.write(content)
+  # print("@@@@@@@@@@이미지 업로드 완료!! @@@@@@@@@@@")
+  ####################################################################################
+  image = ToTensor()(Image.open(file.file))
+  output = model(image)
+  logger.info("Classify Result = {}", output)
+  return {
+      "content_type": file.content_type,
+      "filename": file.filename
+  }
+
+
+
+
+
 
 class Model(BaseModel):
     name: str
     phoneNumber: int
-
-
-app = FastAPI()
 
 @app.get("/")
 def 이름():
@@ -18,39 +50,3 @@ def 이름():
 def data받기(data : Model):
   print(data)
   return '전송완료'
-
-# model = torch.load('your_model.pt')
-# model.eval()
-
-
-@app.post("/photo")
-async def create_upload_file(file: UploadFile = File(...)):
-
-  ################################파일 업로드 부분#####################################
-  UPLOAD_DIR = "./photo"  # 이미지를 저장할 서버 경로
-    
-  content = await file.read()
-  filename = "test.jpg"  # uuid로 유니크한 파일명으로 변경
-  with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
-      fp.write(content)
-  print(file)
-  ####################################################################################
-
-  
-
-  return {
-      "content_type": file.content_type,
-      "filename": file.filename
-  }
-
-
-
-
-# @app.post('/predict')
-# def predict(data: YourInputDataModel):
-#     # 입력 데이터 처리 및 전처리
-#     # ...
-#     # 모델에 입력 데이터 전달하여 예측
-#     output = model(input_data)
-#     # 예측 결과 반환
-#     return {'prediction': output}
