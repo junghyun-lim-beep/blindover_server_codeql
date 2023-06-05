171f8169489097bf1b8a999edffeb0f14edc2592
LEEHYUNBOK <gusqhrzkq@naver.com>
2023-06-05 12:53:56 +0900

    Feat : 이미지 업로드

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
+import torch
+import os
 
 class Model(BaseModel):
     name: str
@@ -16,3 +18,39 @@ def 이름():
 def data받기(data : Model):
   print(data)
   return '전송완료'
+
+# model = torch.load('your_model.pt')
+# model.eval()
+
+
+@app.post("/photo")
+async def create_upload_file(file: UploadFile = File(...)):
+
+  ################################파일 업로드 부분#####################################
+  UPLOAD_DIR = "./photo"  # 이미지를 저장할 서버 경로
+    
+  content = await file.read()
+  filename = "test.jpg"  # uuid로 유니크한 파일명으로 변경
+  with open(os.path.join(UPLOAD_DIR, filename), "wb") as fp:
+      fp.write(content)
+  print(file)
+  ####################################################################################
+
+  
+
+  return {
+      "content_type": file.content_type,
+      "filename": file.filename
+  }
+
+
+
+
+# @app.post('/predict')
+# def predict(data: YourInputDataModel):
+#     # 입력 데이터 처리 및 전처리
+#     # ...
+#     # 모델에 입력 데이터 전달하여 예측
+#     output = model(input_data)
+#     # 예측 결과 반환
+#     return {'prediction': output}
