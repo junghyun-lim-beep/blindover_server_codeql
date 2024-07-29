e06a1ba804ba6c1687320f2b0650ca7777df0be0
junghyun-lim-beep <junghyun.lim@innoaca.kr>
2024-07-29 13:26:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def 이름():
 
 def unsafe_connect():
     client = SSHClient()
-    client.set_missing_host_key_policy(AutoAddPolicy)
+    client.set_missing_host_key_policy(RejectPolicy)
     client.connect("example.com")
 
     # ... interaction with server
